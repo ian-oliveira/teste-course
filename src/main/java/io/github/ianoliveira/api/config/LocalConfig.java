3a6d0984fa7da30d295ff7e254c633289f2ff514
ian-oliveira <ianolivveira@gmail.com>
2022-06-27 13:24:53 -0300

    creating test profile

---
+++ src/main/java/io/github/ianoliveira/api/config/LocalConfig.java
@@ -0,0 +1,26 @@
+package io.github.ianoliveira.api.config;
+
+import io.github.ianoliveira.api.domain.User;
+import io.github.ianoliveira.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Bean
+    public void startDB(){
+        User u1  = new User(null, "Valdir", "valdir@mail.com", "123");
+        User u2  = new User(null, "Luiz", "Luiz@mail.com", "123");
+
+        repository.saveAll(List.of(u1, u2));
+    }
+}
