fc759190a3e43251c9fea6dabaabdd80a80e4feb
ian-oliveira <ianolivveira@gmail.com>
2022-06-27 14:06:49 -0300

    endpoint findAll

--- src/main/java/io/github/ianoliveira/api/resources/UserResource.java
+++ src/main/java/io/github/ianoliveira/api/resources/UserResource.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
@@ -18,10 +21,18 @@ public class UserResource {
     private ModelMapper mapper;
 
     @Autowired
-    private UserService userService;
+    private UserService service;
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll(){
+        return ResponseEntity.ok()
+                .body(service.findAll()
+                        .stream().map(x -> mapper.map(x, UserDTO.class))
+                        .collect(Collectors.toList()));
     }
 }
--- src/main/java/io/github/ianoliveira/api/services/UserService.java
+++ src/main/java/io/github/ianoliveira/api/services/UserService.java
@@ -2,8 +2,11 @@ package io.github.ianoliveira.api.services;
 
 import io.github.ianoliveira.api.domain.User;
 
+import java.util.List;
+
 public interface UserService  {
 
     User findById(Integer id);
 
+    List<User> findAll();
 }
