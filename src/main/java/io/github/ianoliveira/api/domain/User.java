107502d00edd83d892bbb3efb88eaa994b146d33
ian-oliveira <ianolivveira@gmail.com>
2022-06-27 13:24:05 -0300

    switching SpringBoot version to '2.5.5' and excluding Lombok

--- src/main/java/io/github/ianoliveira/api/domain/User.java
+++ src/main/java/io/github/ianoliveira/api/domain/User.java
@@ -1,11 +1,8 @@
 package io.github.ianoliveira.api.domain;
 
-import lombok.*;
-
 import javax.persistence.*;
 
-@Data
-@AllArgsConstructor @NoArgsConstructor
+
 @Entity
 public class User {
 
@@ -17,5 +14,45 @@ public class User {
     private String email;
     private String password;
 
+    public User() {
+    }
 
+    public User(Integer id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
